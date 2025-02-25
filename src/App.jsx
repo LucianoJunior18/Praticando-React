16eed1ba8a4abd97905315ac95087cca18e02a83
junior <junioristylo@gmail.com>
2025-02-25 11:04:06 -0300

    Primeiro commit do meu card com react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from "./components/Profile";
+import img from "./assets/eu.png";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Profile
+        avatar={img}
+        name="Luciano Junior"
+        bio="Front-End Developer"
+        email="Junioristylo@gmail.com"
+        phone="71987416836"
+        githubUrl="https://github.com/LucianoJunior18"
+        likedinUrl="https://www.linkedin.com/in/luciano-oliveira-93389a228/"
+        instagramUrl="https://www.instagram.com/jr_oliveiiraa/"
+      />
+    </div>
+  );
+}
---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import style from "./styles.module.css";
+export default function Profile(props) {
+  return (
+    <div className={style.container}>
+      <img className={style.avatar} src={props.avatar} alt={props.name} />
+      <h2 className={style.name}>{props.name}</h2>
+      <div>{props.bio}</div>
+      <div>{props.phone}</div>
+      <div>{props.email}</div>
+      <div className={style.links}>
+        <a href={props.githubUrl} target="_blank">
+          GitHub
+        </a>
+        <a href={props.likedinUrl} target="_blank">
+          LinkedIn
+        </a>
+        <a href={props.instagramUrl} target="_blank">
+          Instagram
+        </a>
+      </div>
+    </div>
+  );
+}
