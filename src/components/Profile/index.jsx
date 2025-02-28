8553cbedf7def9bd8ca29c32186f3fbaccb0eee2
junior <junioristylo@gmail.com>
2025-02-28 10:27:02 -0300

    adcionando o hulk state no botao de seguir

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,19 +3,30 @@ import LinkButton from "../LinkButton";
 import ProfileSection from "./ProfileSection";
 import style from "./styles.module.css";
 import Title from "../Title";
+import { useState } from "react";
 
-function hanClick() {
-  alert("Voce esta seguindo!!")
-}
+
 
 export default function Profile(props) {
+
+  const [followText, setFollowText] = useState("Follow")
+
+  function hanClick() {
+    alert("Voce esta seguindo!!")
+    setFollowText("Following")
+  }
+
   return (
-    <div className={style.container}>
+
+
+    <div div className={style.container} >
       <img className={style.avatar} src={props.avatar} alt={props.name} />
       <Title>
         <span>{props.name}</span>
-        <button onClick={hanClick} className={style.buttonFollow}>Follow</button>
+        <button onClick={hanClick} className={style.buttonFollow}>{followText}</button>
+
       </Title>
+
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
       <ProfileSection>{props.email}</ProfileSection>
